5b78b8505d8d5e6a4dffda837c1e26455f52d744
Cristian Matas <cristian.matas@gmail.com>
2020-07-27 11:25:10 +0200

    deploying to heroku

--- src/Nuture/settings.py
+++ src/Nuture/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -139,3 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+django_heroku.settings(locals())
